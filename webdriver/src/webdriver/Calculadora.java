80bf6a5d4dfb5e1f39e17bdfb79ad7c3fb2b4e40
danielferrer <danybarco@gmail.com>
2018-01-29 16:09:46 +0100

    Realizar 2 operaciones una tras otra

--- webdriver/src/webdriver/Calculadora.java
+++ webdriver/src/webdriver/Calculadora.java
@@ -1,5 +1,7 @@
 package webdriver;
 
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,6 +14,31 @@ public class Calculadora {
 		driver.get("http://calculadora.net");
 		
 		driver.findElement(By.xpath("//input[@value='2']")).click();
+	    driver.findElement(By.xpath("//input[@value='+']")).click();
+	    driver.findElement(By.xpath("//input[@value='2']")).click();
+	    driver.findElement(By.xpath("//input[@value='=']")).click();
+	    try {
+	        assertEquals("4", driver.findElement(By.id("Display")).getAttribute("value"));
+	      } catch (Error e) {
+	        //verificationErrors.append(e.toString());
+	      }
+	    
+	    //limpiar operación
+	    driver.findElement(By.xpath("//input[@value='C']")).click();
+	    
+	    driver.findElement(By.xpath("//input[@value='3']")).click();
+	    driver.findElement(By.xpath("//input[@value='+']")).click();
+	    driver.findElement(By.xpath("//input[@value='3']")).click();
+	    driver.findElement(By.xpath("//input[@value='=']")).click();
+	    try {
+	        assertEquals("6", driver.findElement(By.id("Display")).getAttribute("value"));
+	      } catch (Error e) {
+	        //verificationErrors.append(e.toString());
+	      }
+	    
+	    assertEquals("5", driver.findElement(By.id("Display")).getAttribute("value"));
 	}
+	
+	
 
 }
