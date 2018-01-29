ade5d6b91f4314f5c31020f3ac49df28fa1d4f2b
danielferrer <danybarco@gmail.com>
2018-01-29 13:29:02 +0100

    Calculadora: pulsar botón 2

--- webdriver/src/webdriver/Calculadora.java
+++ webdriver/src/webdriver/Calculadora.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,7 +9,9 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://calculator.net");
+		driver.get("http://calculadora.net");
+		
+		driver.findElement(By.xpath("//input[@value='2']")).click();
 	}
 
 }
